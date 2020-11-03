cc4bd00e355bf5f4be277465a218446611bba057
dhmec <dharmoradiya@gmail.com>
2020-11-03 14:16:30 -0700

    Add Author, Book, Borrow Class

---
+++ Library_DueDate_Tracker_With_Database/Library_DueDate_Tracker_With_Database/Models/Author.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_DueDate_Tracker_With_Database.Models
+{
+    [Table("author")]
+    public class Author
+    {
+        public Author()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        [Key]
+        [Required]
+        [Column(TypeName = "int(10)")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(60)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
+        
+        [Column(TypeName = "date")]
+        public DateTime DeathDate { get; set; }
+
+        [InverseProperty(nameof(Models.Book.Author))]
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
---
+++ Library_DueDate_Tracker_With_Database/Library_DueDate_Tracker_With_Database/Models/LibraryContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_DueDate_Tracker_With_Database.Models
+{
+    public class LibraryContext : DbContext
+    {
+        //public virtual DbSet<Author> Author  { get; set; }
+        
+    }
+}
