76dd481a4454cbfe352db1dee98c7a0d1b2466f7
dhmec <dharmoradiya@gmail.com>
2020-11-04 22:38:17 -0700

    Add  Details Action in Details view

--- Library_DueDate_Tracker_With_Database/Library_DueDate_Tracker_With_Database/Controllers/BookController.cs
+++ Library_DueDate_Tracker_With_Database/Library_DueDate_Tracker_With_Database/Controllers/BookController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Library_DueDate_Tracker_With_Database.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -25,20 +26,48 @@ namespace Library_DueDate_Tracker_With_Database.Controllers
             ViewBag.Author = AuthorsController.GetAuthors();
             return View();
         }
-        public IActionResult Details()
+        public IActionResult Details(string id)
         {
+            ViewBag.Details = GetBookByID(id);
+
             return View();
         }
+        public IActionResult BorrowBook(string bookId)
+        {
+            ViewBag.BorrowBook = BorrowController.GetBorrowBooks().Where(x=>x.BookID == int.Parse(bookId)).SingleOrDefault();
+
+            return RedirectToAction("Details", new Dictionary<string, string>() { { "id", bookId } });
+        }
+        // Methods
         public static List<Book> GetBooks()
         {
             List<Book> results;
+           
             using (LibraryContext context = new LibraryContext())
             {
-                results = context.Books.Include(x=>x.Author).ToList();
-
+                results = context.Books.Include(x=>x.Author).Include(x=>x.Borrows).ToList();
             }
             return results;
         }
-       
+        public Book GetBookByID(string id)
+        {
+            return GetBooks().Where(x => x.ID == int.Parse(id)).Single();
+        }
+
+        //public void ExtendDueDateByID(string id)
+        //{
+        //    GetBookByID(id).DueDate = GetBookByID(id).DueDate.AddDays(7);
+        //}
+
+        //public void ReturnBookByID(string id)
+        //{
+        //    GetBookByID(id).ReturnedDate = DateTime.Today;
+        //}
+
+        //public void DeleteBookByID(string id)
+        //{
+        //    Books.Remove(GetBookByID(id));
+        //}
+
     }
 }
--- Library_DueDate_Tracker_With_Database/Library_DueDate_Tracker_With_Database/Controllers/BorrowController.cs
+++ Library_DueDate_Tracker_With_Database/Library_DueDate_Tracker_With_Database/Controllers/BorrowController.cs
@@ -4,15 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Library_DueDate_Tracker_With_Database.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 /*
-    Add a “ExtendDueDateForBorrowByID()” method that will extend the “DueDate” by 7 days from today.
-    Add a “ReturnBorrowByID()” method that will set the “Borrow”s “ReturnedDate” to today.
-    Add a “CreateBorrow()” method that will accept a “Book.ID” as a parameter and create a borrow for it.
-    The “CheckOutDate” will be today.
-    The “DueDate” will be 14 days from today.
-    The “ReturnedDate” will be null.
+Add a “ExtendDueDateForBorrowByID()” method that will extend the “DueDate” by 7 days from today.
+Add a “ReturnBorrowByID()” method that will set the “Borrow”s “ReturnedDate” to today.
+Add a “CreateBorrow()” method that will accept a “Book.ID” as a parameter and create a borrow for it.
+The “CheckOutDate” will be today.
+The “DueDate” will be 14 days from today.
+The “ReturnedDate” will be null.
 
- */
+*/
 namespace Library_DueDate_Tracker_With_Database.Controllers
 {
     public class BorrowController : Controller
@@ -24,6 +25,8 @@ namespace Library_DueDate_Tracker_With_Database.Controllers
         }
 
         // Methods
+
+
         public void ExtendDueDateForBorrowByID()
         {
             using (LibraryContext context = new LibraryContext())
@@ -31,6 +34,17 @@ namespace Library_DueDate_Tracker_With_Database.Controllers
                 
             }
             
+        }
+        
+        public static List<Borrow> GetBorrowBooks()
+        {
+            List<Borrow> results;
+            using (LibraryContext context = new LibraryContext())
+            {
+                results = context.Borrows.Include(x=>x.Book).ToList();
+
+            }
+            return results;
         }
     }
 }
